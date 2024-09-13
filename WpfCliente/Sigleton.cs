0ad6967b3c3c71271f47b2bf4608cf20000d355b
Ivan Ali Cerecedo Padilla <zS22013698@estudiantes.uv.mx>
2024-09-13 00:20:48 -0600

    Clase singelton

---
+++ WpfCliente/Sigleton.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfCliente
+{
+    public sealed class Sigleton
+    {
+        private static Sigleton _instance;
+        public static Sigleton Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new Sigleton();
+                }
+                return _instance;
+            }
+        }
+    }
+}

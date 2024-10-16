2726761e44d0082df22f5894bc5f467251ccb2ba
Jose U Naay Mex Loeza <zS22013648@estudiantes.uv.mx>
2024-10-15 22:47:59 -0600

    15/10/2024 U Náay Añadí la estandarización de ventanas emergentes, hay un error grave que no me deja conectarme con la BD, se solucionará mañana

---
+++ WpfCliente/Utilidad/VentanasEmergentes.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfCliente.GUI;
+
+namespace WpfCliente.Utilidad
+{
+    public class VentanasEmergentes
+    {
+        //TODO faltan añadir recursos de los errores
+        public static void CrearVentanaEmergente(string tituloVentanaEmergente, string descripcionVentanaEmergente)
+        {
+            VentanaEmergente ventanaEmergente = new VentanaEmergente(
+                tituloVentanaEmergente,
+                descripcionVentanaEmergente
+            );
+
+            ventanaEmergente.Show();
+        }
+
+        public static void CrearVentanaEmergenteErrorBD()
+        {
+            string tituloVentanaEmergente = Properties.Idioma.tituloErrorBaseDatos;
+            string descripcionVentanaEmergente = Properties.Idioma.errorBaseDatos;
+
+            VentanaEmergente ventanaEmergente = new VentanaEmergente(
+                tituloVentanaEmergente,
+                descripcionVentanaEmergente
+            );
+
+            ventanaEmergente.Show();
+        }
+
+
+        public static void CrearVentanaEmergenteErrorInesperado()
+        {
+            string tituloVentanaEmergente = Properties.Idioma.tituloErrorInesperado;
+            string descripcionVentanaEmergente = Properties.Idioma.errorInesperado;
+
+            VentanaEmergente ventanaEmergente = new VentanaEmergente(
+                tituloVentanaEmergente,
+                descripcionVentanaEmergente
+            );
+
+            ventanaEmergente.Show();
+        }
+
+        public static void CrearVentanaEmergenteLobbyNoEncontrado()
+        {
+            string tituloVentanaEmergente = Properties.Idioma.tituloErrorLobbyNoEncontrado;
+            string descripcionVentanaEmergente = Properties.Idioma.errorLobbyNoEncontrado;
+
+            VentanaEmergente ventanaEmergente = new VentanaEmergente(
+                tituloVentanaEmergente,
+                descripcionVentanaEmergente
+            );
+
+            ventanaEmergente.Show();
+        }
+
+    }
+}
